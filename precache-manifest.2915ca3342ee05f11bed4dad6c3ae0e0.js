bfa34c96a6761944a05695bb34787d3d92b413a1
saksham <saksham.s1203@gmail.com>
2020-10-05 13:26:57 +0530

    Updates

---
+++ precache-manifest.2915ca3342ee05f11bed4dad6c3ae0e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b39a882cdbeed5995b0b6853f5b5ac60",
+    "url": "/covid_19_tracker/index.html"
+  },
+  {
+    "revision": "f6d6de39b66886f9421e",
+    "url": "/covid_19_tracker/static/css/main.74d873a5.chunk.css"
+  },
+  {
+    "revision": "273cf45466153aacd37f",
+    "url": "/covid_19_tracker/static/js/2.e2665d7c.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid_19_tracker/static/js/2.e2665d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d6de39b66886f9421e",
+    "url": "/covid_19_tracker/static/js/main.d96b0a40.chunk.js"
+  },
+  {
+    "revision": "dac72f93dae6ee680d10",
+    "url": "/covid_19_tracker/static/js/runtime-main.2deb42e4.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_19_tracker/static/media/image.d7265326.png"
+  }
+]);
